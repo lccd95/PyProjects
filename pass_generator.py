a89c2c30699084fcde34ecfbf2df2ef48c7c2442
lccd95 <luisaccd@outlook.com>
2024-08-29 12:42:41 +0200

    pass_gen

---
+++ pass_generator.py
@@ -0,0 +1,24 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+#Project below
+
+xl = nr_letters
+xs = nr_symbols 
+xn = nr_numbers
+random_letters = random.choices(letters, k=xl)
+random_symbols = random.choices(symbols, k=xs)
+random_numbers = random.choices(numbers, k=xn)
+
+
+senha_1 = random_letters + random_symbols + random_numbers
+
+random.shuffle(senha_1)
+senha_embaralhada = ''.join(senha_1)
+print(senha_embaralhada)
